378779e3078543a2b3ee728b9418dbc76a568dff
205428178-Firdaus <2025428178@student.uitm.edu.my>
2026-01-01 17:56:00 +0800

    update

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://eutesoiwnhrlajrtufzd.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1dGVzb2l3bmhybGFqcnR1ZnpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMDIwOTksImV4cCI6MjA4MjY3ODA5OX0.C5h1C0JdEKf94MsYFoEhreNT4xa4tq4rGbFhQZ0vBr8',
+};
